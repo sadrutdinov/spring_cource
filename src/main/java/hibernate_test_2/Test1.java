b7eb2812b2dfff664dc87af11dd516c408f821c9
aydar_sadrutdinov <Ajdar18@yandex.ru>
2021-08-24 16:10:34 +0400

    lesson 48. Отношение Один-к-Одному (Uni-directional).

---
+++ src/main/java/hibernate_test_2/Test1.java
@@ -0,0 +1,82 @@
+package hibernate_test_2;
+
+
+import hibernate_test_2.entity.Detail;
+import hibernate_test_2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Employee.class).
+                addAnnotatedClass(Detail.class).
+                buildSessionFactory();
+
+        Session session = null;
+        try {
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Zaur", "Tregulov", "IT", 500);
+//
+//            Detail detail = new Detail("Baku", "123456789", "zaurtregulov@gmail.com");
+//
+//            employee.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Oleg", "Smirnov", "Sales", 700);
+//
+//            Detail detail = new Detail("Moscow", "987654321", "olejka@gmail.com");
+//
+//            employee.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+
+//            session = factory.getCurrentSession();
+//
+//
+//            session.beginTransaction();
+//
+//            Employee employee = session.get(Employee.class, 1);
+//
+//            System.out.println(employee.getEmpDetail());
+//
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+
+
+            session = factory.getCurrentSession();
+
+
+            session.beginTransaction();
+
+            Employee employee = session.get(Employee.class, 2);
+            session.delete(employee);
+
+          //  System.out.println(employee.getEmpDetail());
+
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+
+    }
+}
+

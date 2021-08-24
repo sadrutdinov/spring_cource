ebd2bc20ed1ec5856957619f30ec5d39e82dda94
aydar_sadrutdinov <Ajdar18@yandex.ru>
2021-08-24 13:05:01 +0400

    lesson 43. Получение Java объектов из БД

--- src/main/java/hibernate_test/Test1.java
+++ src/main/java/hibernate_test/Test1.java
@@ -1,5 +1,6 @@
-package hibernate_test.entity;
+package hibernate_test;
 
+import hibernate_test.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -12,11 +13,12 @@ public class Test1 {
         try {
             Session session = factory.getCurrentSession();
 
-            Employee emp = new Employee("Alexandr", "Smirnov", "Sales", 700);
+            Employee emp = new Employee("Alex", "Ivanov", "IT", 600);
 
             session.beginTransaction();
             session.save(emp);
             session.getTransaction().commit();
+            System.out.println(emp);
         } finally {
             factory.close();
         }
---
+++ src/main/java/hibernate_test/Test3.java
@@ -0,0 +1,36 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Test3 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Employee.class).buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+//            List<Employee> emps = session.createQuery("from Employee").getResultList();
+            List<Employee> emps = session.createQuery("from Employee where name = 'Alex' and salary > 500").getResultList();
+
+            for (Employee emp: emps
+                 ) {
+                System.out.println(emp);
+            }
+
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+
+
+    }
+}

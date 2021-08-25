e5a32d79007b1cd015667f279687ddf2e4403642
aydar_sadrutdinov <Ajdar18@yandex.ru>
2021-08-25 11:30:39 +0400

    lesson 50. Отношение Один-ко-Многим (Bi-directional

--- src/main/java/hibernate_one_to_many_bi/Test1.java
+++ src/main/java/hibernate_one_to_many_bi/Test1.java
@@ -1,68 +1,65 @@
-package hibernate_test_2;
+package hibernate_one_to_many_bi;
 
 
-import hibernate_test_2.entity.Detail;
-import hibernate_test_2.entity.Employee;
+
+import hibernate_one_to_many_bi.entity.Department;
+import hibernate_one_to_many_bi.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test2 {
+import java.util.List;
+
+public class Test1 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").
                 addAnnotatedClass(Employee.class).
-                addAnnotatedClass(Detail.class).
+                addAnnotatedClass(Department.class).
                 buildSessionFactory();
 
         Session session = null;
         try {
-
-//            session = factory.getCurrentSession();
-//
-//            Employee employee = new Employee("Nikolay", "Ivanov", "HR", 850);
-//
-//            Detail detail = new Detail("New-York", "911111", "kolya@gmail.com");
-//            employee.setEmpDetail(detail);
-//            detail.setEmployee(employee);
-//
-//
-//            session.beginTransaction();
-//            session.save(detail);
-//
-//
-//
-//            session.getTransaction().commit();
-//            System.out.println("Done!");
-//
 //            session = factory.getCurrentSession();
 //
 //
 //
+//            Department department = new Department("IT", 300, 1200);
+//
+//            Employee empl1 = new Employee("Zaur", "Tregulov", 800);
+//            Employee empl2 = new Employee("Elena", "Smirnova", 1000);
+//
+//            department.addEmployeeToDepartment(empl1);
+//            department.addEmployeeToDepartment(empl2);
 //
 //            session.beginTransaction();
-//
-//            Detail detail = session.get(Detail.class, 4);
-//
-//            System.out.println(detail.getEmployee());
-//
-//
-//
+//            session.save(department);
 //
 //            session.getTransaction().commit();
 //            System.out.println("Done!");
 
+//            session = factory.getCurrentSession();
+//
+//
+//            session.beginTransaction();
+//            Department it = session.get(Department.class, 1);
+//
+//            List<Employee> emps = it.getEmps();
+//
+//            for (Employee emp: emps
+//                 ) {
+//                System.out.println(emp);
+//            }
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
 
             session = factory.getCurrentSession();
 
 
-
-
             session.beginTransaction();
+            Employee employee = session.get(Employee.class, 2);
 
-            Detail detail = session.get(Detail.class, 1);
-            detail.getEmployee().setEmpDetail(null);
-            session.delete(detail);
-
+            session.delete(employee);
 
 
 
--- src/main/java/hibernate_one_to_many_bi/entity/Employee.java
+++ src/main/java/hibernate_one_to_many_bi/entity/Employee.java
@@ -1,4 +1,6 @@
-package hibernate_test_2.entity;
+package hibernate_one_to_many_bi.entity;
+
+import hibernate_one_to_one.entity.Detail;
 
 import javax.persistence.*;
 
@@ -17,37 +19,23 @@ public class Employee {
     @Column(name = "surname")
     private String surname;
 
-    @Column(name = "department")
-    private String department;
 
     @Column(name = "salary")
     private int salary;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "details_id")
-    private Detail empDetail;
-
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.MERGE, CascadeType.DETACH})
+    @JoinColumn(name = "department_id")
+    private Department department;
 
     public Employee() {
     }
 
-    public Employee(String name, String surname, String department, int salary) {
+    public Employee(String name, String surname, int salary) {
         this.name = name;
         this.surname = surname;
-        this.department = department;
         this.salary = salary;
     }
 
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", department='" + department + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
 
     public int getId() {
         return id;
@@ -73,14 +61,6 @@ public class Employee {
         this.surname = surname;
     }
 
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
     public int getSalary() {
         return salary;
     }
@@ -89,11 +69,21 @@ public class Employee {
         this.salary = salary;
     }
 
-    public Detail getEmpDetail() {
-        return empDetail;
+    public Department getDepartment() {
+        return department;
     }
 
-    public void setEmpDetail(Detail empDetail) {
-        this.empDetail = empDetail;
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
     }
 }
